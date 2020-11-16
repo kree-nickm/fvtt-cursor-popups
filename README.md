058d6f617582bfa7bb780181ff897e6989ef225e
Nick <38154033+kree-nickm@users.noreply.github.com>
2020-11-16 10:31:00 +0000

    added arrow

--- README.md
+++ README.md
@@ -5,6 +5,5 @@ Causes a predetermined icon to appear at your cursor when you hold down a key, v
 Pressing the key replaces the normal player cursors that are usually visible, so those must be enabled in order for a player to use this feature.
 
 ## Future Changes
-* Additional icons aside from just a speech bubble. MapTool also has a thought bubble, an arrow, and pointer finger. I at least want to add the arrow.
 * Allow the GM to add their own popup icons, either through an image or by using the canvas drawing methods.
 * Make the popup more responsive when it's visible. Right now the cursor doesn't follow the user's actual mouse movements, which is fine normally, but might get confusing if it results in the popup being in the wrong place.
--- cursor-popups.js
+++ cursor-popups.js
@@ -1,18 +1,107 @@
 "use strict";
 
-Hooks.once("init", () => {
-   game.settings.register("cursor-popups", "keyPress", {
-      name: "Speech Bubble Key",
-      hint: "The key you press to make the speech bubble appear.",
-      scope: "client",
-      config: true,
+const cursorIcons = [
+   {
+      name: "Speech Bubble",
       default: "KeyT",
-      type: String,
-   });
+      draw: function(g, x=0, y=0, s=1)
+      {
+         g.beginFill(0xFFFFFF, 0.8).lineStyle(1, 0x000000, 1)
+            .moveTo(x, y)
+            .lineTo(x+6*s, y-13*s)
+            .bezierCurveTo(x-20*s, y-35*s, x+46*s, y-67*s, x+90*s, y-40*s)
+            .bezierCurveTo(x+125*s, y-18*s, x+61*s, y+15*s, x+18*s, y-6*s)
+            .lineTo(x, y)
+            .closePath();
+      },
+   },
+   {
+      name: "Arrow",
+      default: "KeyY",
+      draw: function(g, x=0, y=0, s=1)
+      {
+         g.beginFill(0xFFFFFF, 0.8).lineStyle(1, 0x000000, 1)
+            .moveTo(x, y)
+            .lineTo(x+15*s, y-18*s)
+            .lineTo(x+7*s, y-18*s)
+            .lineTo(x+7*s, y-50*s)
+            .lineTo(x-7*s, y-50*s)
+            .lineTo(x-7*s, y-18*s)
+            .lineTo(x-15*s, y-18*s)
+            .lineTo(x, y)
+            .closePath();
+      },
+   },
+];
+
+// TODO: Every Foundry VTT update, check to make sure these ControlsLayer functions we are overwriting didn't change.
+ControlsLayer.prototype.drawCursors = function()
+{
+   if(this.cursors)
+   {
+      this.cursors.destroy({children: true});
+      this.cursors = null;
+   }
+   this.cursors = this.addChild(new PIXI.Container());
+   for (let u of game.users.entities.filter(u => u.active))
+   {
+      let cursor = this.drawCursor(u);
+      cursor.dot = cursor.children[0];
+      cursor.name = cursor.children[1];
+      for(let icon of cursorIcons)
+      {
+         cursor[`icon${icon.slug}`] = cursor.addChild(new PIXI.Graphics());
+         icon.draw(cursor[`icon${icon.slug}`], 0, 0, 2);
+         cursor[`icon${icon.slug}`].visible = false;
+      }
+      cursor.dot.visible = u !== game.user;
+      cursor.name.visible = u !== game.user;
+   }
+};
+
+ControlsLayer.prototype.updateCursor = function(user, position) {
+   if (!this.cursors)
+      return;
+   const cursor = this._cursors[user._id] || this.drawCursor(user);
+
+   // Ignore cursors on other Scenes
+   if((position === null) || (user.viewedScene !== canvas.scene._id))
+   {
+      if(cursor)
+         cursor.visible = false;
+      return;
+   }
+
+   // Ignore cursors for users who are not permitted to share
+   if(user === game.user && !game.settings.get("cursor-popups", "showSelf") || !user.hasPermission("SHOW_CURSOR"))
+   {
+      if(cursor)
+         cursor.visible = false;
+      return;
+   }
+
+   // Show the cursor in its currently tracked position
+   cursor.visible = true;
+   cursor.target = {x: position.x || 0, y: position.y || 0};
+};
+
+Hooks.once("init", () => {
+   for(let icon of cursorIcons)
+   {
+      icon.slug = icon.name.slugify("", true);
+      game.settings.register("cursor-popups", `${icon.slug}Key`, {
+         name: `${icon.name} Key`,
+         hint: `The key you press to make the ${icon.name.toLowerCase()} appear.`,
+         scope: "client",
+         config: true,
+         default: icon.default,
+         type: String,
+      });
+   }
    
    game.settings.register("cursor-popups", "showSelf", {
-      name: "Show Your Own Bubble",
-      hint: "Should your own bubble be shown?",
+      name: "Show Your Own Icon?",
+      hint: "Should your own icons be shown to you?",
       scope: "client",
       config: true,
       default: false,
@@ -20,126 +109,72 @@ Hooks.once("init", () => {
    });
 });
 
-// canvasInit is the last hook that fires before ControlsLayer is drawn, so we edit the prototype now.
-Hooks.once("canvasInit", () => {
-   // Draws the speech bubble into the PIXI.Graphics object `graphics` with offset `x`,`y` and size multiplier `s`.
-   function drawSpeechBubble(graphics, x=0, y=0, s=1)
-   {
-      graphics.beginFill(0xFFFFFF, 0.8).lineStyle(1, 0x000000, 1)
-         .moveTo(x, y)
-         .lineTo(x+6*s, y-13*s)
-         .bezierCurveTo(x-20*s, y-35*s, x+46*s, y-67*s, x+90*s, y-40*s)
-         .bezierCurveTo(x+125*s, y-18*s, x+61*s, y+15*s, x+18*s, y-6*s)
-         .lineTo(x, y)
-         .closePath();
-   }
-   
-   // TODO: Every foundry update, check to make sure these ControlsLayer functions we are overwriting didn't change.
-   ControlsLayer.prototype.drawCursors = function()
-   {
-      if(this.cursors)
-      {
-         this.cursors.destroy({children: true});
-         this.cursors = null;
-      }
-      this.cursors = this.addChild(new PIXI.Container());
-      for (let u of game.users.entities.filter(u => u.active && (u._id !== game.user._id || game.settings.get("cursor-popups", "showSelf"))))
-      {
-         let cursor = this.drawCursor(u);
-         cursor.dot = cursor.children[0];
-         cursor.name = cursor.children[1];
-         cursor.speechBubble = cursor.addChild(new PIXI.Graphics());
-         drawSpeechBubble(cursor.speechBubble, 0, 0, 2);
-         cursor.dot.visible = u !== game.user;
-         cursor.name.visible = u !== game.user;
-         cursor.speechBubble.visible = false;
-      }
-   };
-   
-   ControlsLayer.prototype.updateCursor = function(user, position) {
-      if (!this.cursors)
-         return;
-      const cursor = this._cursors[user._id] || this.drawCursor(user);
-
-      // Ignore cursors on other Scenes
-      if((position === null) || (user.viewedScene !== canvas.scene._id))
-      {
-         if(cursor)
-            cursor.visible = false;
-         return;
-      }
-
-      // Ignore cursors for users who are not permitted to share
-      if(user === game.user && !game.settings.get("cursor-popups", "showSelf") || !user.hasPermission("SHOW_CURSOR"))
-      {
-         if(cursor)
-            cursor.visible = false;
-         return;
-      }
-
-      // Show the cursor in its currently tracked position
-      cursor.visible = true;
-      cursor.target = {x: position.x || 0, y: position.y || 0};
-   };
-});
-
 Hooks.once("ready", async () => {
-   // There's a KeyboardManager class that handles Foundry's keybinds, but it's pretty much useless to modules. Maybe one day it will be worthwhile.
+   // There's a KeyboardManager class that handles Foundry's keybinds, but it's pretty much useless to modules.
    window.addEventListener("keydown", event => {
       // Make sure we aren't typing into a text field or something.
       if(event.target.nodeName === "BODY")
       {
-         let keys = game.settings.get("cursor-popups", "keyPress").split("+");
-         if(event.code === keys[keys.length-1]
-            && (keys.indexOf("Shift") == -1 || event.shiftKey)
-            && (keys.indexOf("Control") == -1 || event.ctrlKey)
-            && (keys.indexOf("Alt") == -1 || event.altKey)
-            && (keys.indexOf("Meta") == -1 || event.metaKey))
+         for(let icon of cursorIcons)
          {
-            event.stopPropagation();
-            event.preventDefault();
-            game.user.update({showSpeechBubble:true});
+            let key = game.settings.get("cursor-popups", `${icon.slug}Key`).split("+");
+            if(event.code === key[key.length-1]
+               && (key.indexOf("Shift") == -1 || event.shiftKey)
+               && (key.indexOf("Control") == -1 || event.ctrlKey)
+               && (key.indexOf("Alt") == -1 || event.altKey)
+               && (key.indexOf("Meta") == -1 || event.metaKey))
+            {
+               event.stopPropagation();
+               event.preventDefault();
+               game.user.update({[`show${icon.slug}`]:true});
+            }
          }
       }
    });
    
    window.addEventListener("keyup", event => {
-      // Check to see if we even need to update before executing any further code.
-      if(game.user.data.showSpeechBubble)
+      for(let icon of cursorIcons)
       {
-         let keys = game.settings.get("cursor-popups", "keyPress").split("+");
-         if(event.code === keys[keys.length-1])
+         if(game.user.data[`show${icon.slug}`])
          {
-            game.user.update({showSpeechBubble:false});
+            let keys = game.settings.get("cursor-popups", `${icon.slug}Key`).split("+");
+            if(event.code === keys[keys.length-1])
+            {
+               game.user.update({[`show${icon.slug}`]:false});
+            }
          }
       }
    });
 });
 
 Hooks.on("updateUser", async (user, data, options, userId) => {
-   // If the speech bubble isn't being toggled, we don't need to do anything.
-   if(data.hasOwnProperty("showSpeechBubble"))
+   for(let icon of cursorIcons)
    {
-      if(data.showSpeechBubble)
+      if(data.hasOwnProperty(`show${icon.slug}`))
       {
-         if(canvas.controls._cursors[userId])
+         if(data[`show${icon.slug}`])
          {
-            canvas.controls._cursors[userId].dot.visible = false;
-            canvas.controls._cursors[userId].name.visible = false;
-            canvas.controls._cursors[userId].speechBubble.visible = true;
+            if(canvas.controls._cursors[userId])
+            {
+               canvas.controls._cursors[userId].dot.visible = false;
+               canvas.controls._cursors[userId].name.visible = false;
+               for(let icon2 of cursorIcons)
+                  canvas.controls._cursors[userId][`icon${icon2.slug}`].visible = false;
+               canvas.controls._cursors[userId][`icon${icon.slug}`].visible = true;
+            }
+            else
+            {
+               console.warn("Current user has no cursor.");
+            }
          }
          else
          {
-            console.warn("Current user has no cursor.");
-         }
-      }
-      else
-      {
-         if(canvas.controls._cursors[userId])
-         {
-            canvas.controls._cursors[userId].dot.visible = user !== game.user;
-            canvas.controls._cursors[userId].name.visible = user !== game.user;
-            canvas.controls._cursors[userId].speechBubble.visible = false;
+            if(canvas.controls._cursors[userId])
+            {
+               canvas.controls._cursors[userId].dot.visible = user !== game.user;
+               canvas.controls._cursors[userId].name.visible = user !== game.user;
+               canvas.controls._cursors[userId][`icon${icon.slug}`].visible = false;
+            }
          }
       }
    }
@@ -151,9 +186,9 @@ Hooks.on("canvasReady", async () => {
       if(game.settings.get("cursor-popups", "showSelf"))
       {
          // Throttle cursor position updates to 100ms per tick
-         let now = Date.now();
+         /*let now = Date.now();
          if ((now - lastCursor) < 100) return;
-         lastCursor = now;
+         lastCursor = now;*/
          
          let coords = event.data.getLocalPosition(canvas.controls);
          if(coords)
@@ -166,8 +201,7 @@ Hooks.on("canvasReady", async () => {
 
 // Set up the keybind field(s) to function like traditional keybind options instead of text fields.
 Hooks.on("renderSettingsConfig", async () => {
-   let keyPress = $("#config-tabs input[name='cursor-popups.keyPress']");
-   keyPress[0].placeholder = "Press Desired Key";
+   let keyPress = $("#config-tabs input[name^='cursor-popups.']");
    keyPress.keydown(event => {
       event.stopPropagation();
       event.preventDefault();
@@ -189,13 +223,13 @@ Hooks.on("renderSettingsConfig", async () => {
       }
    });
    keyPress.focus(event => {
-      event.currentTarget.defaultValue = event.currentTarget.value;
       event.currentTarget.value = "";
+      event.currentTarget.placeholder = "Press Desired Key";
    });
    keyPress.blur(event => {
       if(event.currentTarget.value == "")
       {
-         event.currentTarget.value = event.currentTarget.defaultValue;
+         event.currentTarget.placeholder = "None";
       }
    });
 });
